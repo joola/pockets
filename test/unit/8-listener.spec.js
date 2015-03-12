9d2d26f113ecbc9e538678a938e6d3bdcf8be07e
Itay Weinberger <itay@joo.la>
2015-03-13 01:08:52 +0200

    #12 moved listener test to bitcoin.

--- test/unit/8-listener.spec.js
+++
@@ -1,7 +0,0 @@
-describe("listener", function () {
-  xit("should listen for a wallet update", function (done) {
-    engine.listener.add('mpSKAvSLeZTHbstNEzYKs7fxw9fat6a1Y5');
-    engine.events.on('wallet-update',done);
-    //now send the money
-  });
-});
