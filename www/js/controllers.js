ce27c51742f9e61fcdc2a530cdfab3c6e73b603a
orweinberger <orweinberger@gmail.com>
2015-03-12 20:05:20 +0200

    Added homepage

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,28 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('homeCtrl', function($scope, $state) {
+    $scope.start = function() {
+      console.log('clicked');
+      //$state.go('persona.tab');
+    };
+  })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+  .controller('DashCtrl', function ($scope) {
+  })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    }
+  })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
