df28b8cadd0fa847429742586e57bbd148a84de2
Itay Weinberger <itay@joo.la>
2015-03-12 23:36:25 +0200

    #14 more pocket functionality.

--- test/unit/7-pockets.spec.js
+++ test/unit/7-pockets.spec.js
@@ -1,13 +1,42 @@
 describe("pockets", function () {
-  it("should validate a address", function (done) {
-    engine.bitcoin.validateWallet({address: 'mpSKAvSLeZTHbstNEzYKs7fxw9fat6a1Y5'}, done);
+  it("should add a pocket", function (done) {
+    engine.pockets.create({
+      name: 'root',
+      description: ''
+    }, done);
   });
 
-  it("should fail validating an invalid address", function (done) {
-    engine.bitcoin.validateWallet({address: 'mpSKAvSLeZTHbstNEzYKs7fxw9fat6a1Y1'}, function (err) {
+  it("should update a pocket", function (done) {
+    engine.pockets.update({
+      name: 'root',
+      description: 'test'
+    }, function (err) {
       if (err)
-        return done();
-      return done(new Error('This should fail'));
+        return done(err);
+
+      expect(engine.pockets.ROOT.description).to.equal('test');
+      done();
+    });
+  });
+
+  it("should create a 1st level pocket", function (done) {
+    engine.pockets.create({
+      name: 'root',
+      description: ''
+    }, function (err) {
+      if (err)
+        return done(err);
+
+      engine.pockets.create({
+        parent: 'root',
+        name: 'savings'
+      }, function (err) {
+        if (err)
+          return done(err);
+
+        expect(engine.pockets.ROOT.pockets.savings).to.be.ok;
+        done();
+      });
     });
   });
 });
